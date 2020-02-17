8ef66864d0710128312f509b0428e366d371be52
Mateus Rocha Pinto <43857659+mateusrp44@users.noreply.github.com>
2020-02-17 14:11:26 -0300

    Encapsulamento

--- prjC++OrientadoObjeto/main.cpp
+++ prjC++OrientadoObjeto/main.cpp
@@ -6,23 +6,37 @@ using namespace std;
 
 class Carro
 {
-    public:
+    private:
         string marca;
         int ano;
+    public:
+        void setMarca(string x) {
+            marca = x;
+        }
+        string getMarca() {
+            return marca;
+        }
+        void setAno(int y) {
+            if(y > 1990) ano = y;
+            else ano = 1990;
+        }
+        int getAno() {
+            return ano;
+        }
 };
 
 main()
 {
     Carro c1;
-    c1.marca = "Fiat";
-    c1.ano = 2017;
+    c1.setMarca("Fiat");
+    c1.setAno(2017);
     
     Carro c2;
-    c2.marca = "Ferrari";
-    c2.ano = 2015;
+    c2.setMarca("Ferrari");
+    c2.setAno(1989);
     
-    cout << c1.marca << endl;
-    cout << c1.ano << endl;
-    cout << c2.marca << endl;
-    cout << c2.ano << endl;
+    cout << c1.getMarca() << endl;
+    cout << c1.getAno() << endl;
+    cout << c2.getMarca() << endl;
+    cout << c2.getAno() << endl;
 }
